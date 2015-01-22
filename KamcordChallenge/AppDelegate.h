ffbed3a99c38bc176d0fa1e3df5d0987b5fb08f5
Jonathan Kim <jonnokim17@gmail.com>
2015-01-22 11:37:03 -0800

    Initial Commit

---
+++ KamcordChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KamcordChallenge
+//
+//  Created by Jonathan Kim on 1/22/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KamcordChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KamcordChallenge
+//
+//  Created by Jonathan Kim on 1/22/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
